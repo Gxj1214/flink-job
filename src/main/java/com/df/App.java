4d13b018df2fd9d20825e86c793f0cc3d9c0b777
Gxj1214 <57927942+Gxj1214@users.noreply.github.com>
2021-12-05 17:35:37 +0800

    fix fetch

--- src/main/java/com/df/App.java
+++ src/main/java/com/df/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World dev dev2!" );
-        System.out.println( "Hello World dev master!" );
+        System.out.println( "Hello World dev master fetch!" );
     }
 }
